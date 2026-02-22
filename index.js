cba05c0d4300be2cdd87860fc7bcdb32ad422655
baltagcristian666-beep <baltagcristian666@gmail.com>
2026-02-22 16:45:02 +0200

    db

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+  import { getDatabase } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-database.js";
+
+  const firebaseConfig = {
+        databaseURL: "https://leads-tracker-afbc0-default-rtdb.europe-west1.firebasedatabase.app/"
+  };
+
+  const app = initializeApp(firebaseConfig)
+  const database  = getDatabase(app)
+
+  console.log(database)
+
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
